a7dfcd8ab43484132905d4c510242402429e7e51
jakeb-k <jknow30@outlook.com>
2023-12-03 18:20:33 +1000

    scss compile

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <link rel="stylesheet" href="{{asset('css/app.scss')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}" type="text/css">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
