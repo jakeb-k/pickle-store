20635fbd951f865d3061a6dc8107e654c29ee547
jakeb-k <jknow30@outlook.com>
2024-04-02 14:15:00 +1000

    created product factory for testing

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product; 
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+   /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->word . $this->faker->numberBetween(1, 10000),
+            'url' => $this->faker->url,
+            'type' => $this->faker->word,
+            'delivery' => $this->faker->numberBetween(1, 10),
+            'price' => $this->faker->randomFloat(2, 1, 1000),
+            'discount' => $this->faker->randomFloat(2, 1, 100) ?: null,
+            'sku' => $this->faker->uuid ?: null,
+            'description' => $this->faker->sentence ?: null,
+            'image' => $this->faker->imageUrl() ?: null,
+            'tags' => $this->faker->word,
+            'rating' => $this->faker->randomFloat(1, 1, 5) ?: null,
+            'available' => $this->faker->boolean,
+            'options' => $this->faker->sentence ?: null,
+        ];
+    }
+}
